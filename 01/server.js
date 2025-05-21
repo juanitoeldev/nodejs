dd04cf56d5266a9f0e341513c7d544bd7cffeab6
juanito <juanitoeldev@gmail.com>
2025-05-21 08:36:33 +0000

    Servidor HTTP básico hecho en ESM y CommonJS

---
+++ 01/server.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http');
+// configuramos el servidor
+const hostname = 'localhost';
+const port = 3000;
+// creamos el servidor
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+// iniciamos el servidor
+server.listen(port, hostname, () => {
+  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
